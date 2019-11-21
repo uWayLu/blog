214c976b1e62413bfe30a00022f4b564cc5d1743
Webb <u.master.o.twn@gmail.com>
2019-11-21 13:30:06 +0800

    docs: update review and dir

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,7 @@ module.exports = {
     host: 'localhost',
     port: '8042',
     extraWatchFiles: [],
+    permalink: "/blog/:year/:month/:day/:slug",
     themeConfig: {
         nav: [
             {
@@ -11,7 +12,7 @@ module.exports = {
                 ariaLabel: 'Wish List Menu',
                 items: [
                     {
-                        text: 'Food', link: 'https://www.google.com/search?q=台灣%20美食',
+                        text: 'Food', link: 'https://www.notion.so/06e56a34024e499eb4f9e4266df47140?v=1dbb15047dc94827894f76116265922e',
                     }
                 ]
             },
@@ -30,7 +31,7 @@ module.exports = {
             {
                 text: '未分類',
                 ariaLabel: '未分類文章',
-                link: '/uncategorized/天氣之子觀後感'
+                link: '/blog/2019/10/07/天氣之子觀後感/'
             }
         ],
         sidebar: 'auto'
