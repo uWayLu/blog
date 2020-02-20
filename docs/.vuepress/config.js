9aaf565f5bc4ed5664ab3c249ec22b0e7b0a5623
webblu <Webb Lu>
2020-02-20 18:53:15 +0800

    (wip) write hello blog

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,46 +4,60 @@ module.exports = {
   host: "localhost",
   port: "8042",
   extraWatchFiles: [],
-  permalink: "/blog/:year/:month/:day/:slug",
   themeConfig: {
-    nav: [
-      {
-        text: "Wish List",
-        ariaLabel: "Wish List Menu",
-        items: [
-          {
-            text: "Food",
-            link:
-              "https://www.notion.so/06e56a34024e499eb4f9e4266df47140?v=1dbb15047dc94827894f76116265922e"
-          }
-        ]
-      },
-      {
-        text: "Side Project",
-        ariaLabel: "Side Project Menu",
-        items: [
-          {
-            text: "Linebot",
-            link: "#"
-          },
-          {
-            text: "Webb Site",
-            link: "#"
-          }
-        ]
-      },
-      {
-        text: "未分類",
-        ariaLabel: "未分類文章",
-        link: "/blog/2019/10/07/天氣之子觀後感/"
-      }
-    ],
+    // nav: [
+    //   {
+    //     text: "Wish List",
+    //     ariaLabel: "Wish List Menu",
+    //     items: [
+    //       {
+    //         text: "Food",
+    //         link:
+    //           "https://www.notion.so/06e56a34024e499eb4f9e4266df47140?v=1dbb15047dc94827894f76116265922e"
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     text: "Side Project",
+    //     ariaLabel: "Side Project Menu",
+    //     items: [
+    //       {
+    //         text: "Linebot",
+    //         link: "#"
+    //       },
+    //       {
+    //         text: "Webb Site",
+    //         link: "#"
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     text: "未分類",
+    //     ariaLabel: "未分類文章",
+    //     link: "/blog/2019/10/07/天氣之子觀後感/"
+    //   }
+    // ],
     sidebar: "auto"
   },
   plugins: [
-    "@vuepress/blog",
-    {
-      /* options */
-    }
+    [
+      "@vuepress/blog",
+      {
+        directories: [
+          {
+            id: "post",
+            dirname: "blog",
+            path: "/",
+            itemPermalink: "/:year/:month/:day/:slug"
+          },
+          // {
+          // id: "archived",
+          // dirname: "blog/uncategorized",
+          // path: "/archived/",
+          // itemPermalink: "/archived/:year/:month/:day/:slug"
+          // },
+        ]
+      }
+    ]
   ]
 };
---
+++ docs/blog/2020-02-20-hello-blog.md
@@ -0,0 +1,17 @@
+---
+title: Hello Blog
+date: 2020-02-20
+tags: 
+  - Blog
+  - Vuepress
+  - Config
+author: Webb Lu
+location: Kaohsiung, Taiwan
+---
+
+## Hello Blog
+
++ [x] Init with vuepress
++ [x] Init vuepress/plugin-blog
++ [ ] Init vurpress/theme-blog
++ [ ] ...
