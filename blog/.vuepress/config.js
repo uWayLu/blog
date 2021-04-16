eb476ea8750f84f9f00891feb2487f2942716646
WebbLu <u.master.o.twn@gmail.com>
2021-04-16 13:14:24 +0800

    chore: update nav

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -7,38 +7,45 @@ module.exports = {
   extraWatchFiles: [],
   theme: "@vuepress/theme-blog",
   themeConfig: {
-    // nav: [
-    //   {
-    //     text: "Wish List",
-    //     ariaLabel: "Wish List Menu",
-    //     items: [
-    //       {
-    //         text: "Food",
-    //         link:
-    //           "https://www.notion.so/06e56a34024e499eb4f9e4266df47140?v=1dbb15047dc94827894f76116265922e"
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     text: "Side Project",
-    //     ariaLabel: "Side Project Menu",
-    //     items: [
-    //       {
-    //         text: "Linebot",
-    //         link: "#"
-    //       },
-    //       {
-    //         text: "Webb Site",
-    //         link: "#"
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     text: "未分類",
-    //     ariaLabel: "未分類文章",
-    //     link: "/_posts/2019/10/07/天氣之子觀後感/"
-    //   }
-    // ],
+    nav: [
+      // {
+      //   text: "Wish List",
+      //   ariaLabel: "Wish List Menu",
+      //   items: [
+      //     {
+      //       text: "Food",
+      //       link:
+      //         "https://www.notion.so/06e56a34024e499eb4f9e4266df47140?v=1dbb15047dc94827894f76116265922e"
+      //     }
+      //   ]
+      // },
+      // {
+      //   text: "Side Project",
+      //   ariaLabel: "Side Project Menu",
+      //   items: [
+      //     {
+      //       text: "Linebot",
+      //       link: "#"
+      //     },
+      //     {
+      //       text: "Webb Site",
+      //       link: "#"
+      //     }
+      //   ]
+      // },
+      {
+        text: "Webb Page",
+        link: "https://uwaylu.github.io"
+      },
+      {
+        text: 'Blog',
+        link: '/',
+      },
+      {
+        text: 'Tags',
+        link: '/tag/',
+      },
+    ],
     sidebar: "auto",
   },
   plugins: [
